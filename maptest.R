09ef92a8d26ab8e52e9d973c80f4c3a8075f8bd9
Julianne Buggs <jbugg005@fiu.edu>
2024-02-20 08:25:19 -0500

    Begin "Drawing beautiful maps..." tutorial

--- maptest.R
+++ maptest.R
@@ -1,2 +1,13 @@
-library(tidyverse)
-install.packages("ggplot2")
+##Drawing beautiful maps programmatically with R, sf, and ggplot2
+#Part 1: Basics
+install.packages(c("cowplot", "googleway", "ggplot2", "ggrepel",
+                   "ggspatial", "libwgeom", "sf", "rnaturalearth",
+                   "rnaturalearthdata"))
+library("ggplot2")
+theme_set(theme_bw())
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
